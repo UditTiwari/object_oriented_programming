bb412906fe0af144e45c3b3ecc77d952e94b7abd
udit_tiwari <udit.tiwari@quantive.com>
2023-07-25 17:12:46 +0530

    25 july magic and dunder

---
+++ magic_and_dunder.py
@@ -0,0 +1,25 @@
+class Book():
+    def __init__(self,title,author,pages) -> None:
+        self.title = title
+        self.author = author
+        self.pages = pages
+        
+    def __str__(self) -> str:
+        return f"{self.title} by {self.author}"
+    
+    def __len__(self):
+        return self.pages
+    
+    def __del__(self):
+        return ("A book object has been deleted")
+        
+        
+
+b = Book('Python rocks','Jose',200)
+
+print(b)
+
+# print(len(b))
+
+print(b)
+
